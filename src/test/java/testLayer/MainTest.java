81bf4ed1e65863281cb9f66d1cb8e7d48171942e
prafpawar11 <prafulp1030@gmail.com>
2025-07-05 07:51:14 +0530

    Home Page TC by Suraj

--- src/test/java/testLayer/MainTest.java
+++ src/test/java/testLayer/MainTest.java
@@ -1,17 +1,20 @@
 package testLayer;
 
+import pages.HomePage;
 import pages.LoginPage;
 
 public class MainTest {
 
 	public static void main(String[] args) {
-		
-		LoginPage login =new LoginPage();
-		
+
+		LoginPage login = new LoginPage();
+
 		login.loginFunctionality("Admin", "admin123");
-		
-		
-		
+
+		HomePage home = new HomePage();
+
+		home.validateLogo();
+
 	}
 
 }
